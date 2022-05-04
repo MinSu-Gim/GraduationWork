ce6e2a8860dcc7b6c3edb00cb30f8ddf2d2493f2
MinSu-Gim <MinSu_Kim@outlook.kr>
2022-05-04 17:44:09 +0900

    Store & menu 저장 기능

---
+++ src/main/java/com/brother/graduationwork/service/StoreService.java
@@ -0,0 +1,11 @@
+package com.brother.graduationwork.service;
+
+import com.brother.graduationwork.domain.Menu;
+import com.brother.graduationwork.domain.Store;
+
+public interface StoreService {
+
+    Long registerStore(Store store);
+
+    Long registerMenu(Menu menu);
+}
---
+++ src/main/java/com/brother/graduationwork/service/StoreServiceImpl.java
@@ -0,0 +1,31 @@
+package com.brother.graduationwork.service;
+
+import com.brother.graduationwork.domain.Menu;
+import com.brother.graduationwork.domain.Store;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Slf4j
+@Repository
+@Transactional
+public class StoreServiceImpl implements StoreService {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Override
+    public Long registerStore(Store store) {
+        em.persist(store);
+        return store.getId();
+    }
+
+    @Override
+    public Long registerMenu(Menu menu) {
+        em.persist(menu);
+        return menu.getId();
+    }
+}
