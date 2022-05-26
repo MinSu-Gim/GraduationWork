69de3442f3f720c377cad5fc7e0d7ed979c37762
MinSu-Gim <MinSu_Kim@outlook.kr>
2022-05-27 02:26:13 +0900

    WebSocketConfig Settings

--- src/main/java/com/brother/graduationwork/config/WebConfig.java
+++ src/main/java/com/brother/graduationwork/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.brother.graduationwork;
+package com.brother.graduationwork.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
